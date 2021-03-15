f0c7756c0af925b994dd119478e998768eb44a95
Rodion <45131611+Rodion-progs@users.noreply.github.com>
2021-03-15 04:02:16 +0300

    Add files via upload

---
+++ nuxt/static/1615769593/admin/new-post/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-blog\u002F_nuxt\u002Fstatic\u002F1615769593",layout:"admin",error:a,state:a,serverRendered:false,routePath:"\u002Fadmin\u002Fnew-post",config:{app:{basePath:"\u002Fnuxt-blog\u002F",assetsPath:"\u002Fnuxt-blog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
